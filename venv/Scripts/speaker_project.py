9460c5f2368dd05e8ab9f3ee3785c89e48c7d327
bruno_gaddini <bruno_gaddini@hotmail.com>
2023-03-29 22:51:29 -0300

    .

--- venv/Scripts/speaker_project.py
+++ venv/Scripts/speaker_project.py
@@ -11,3 +11,14 @@ pdf = pb.open('O elefante em apuros.pdf')
 
 # Generating the book page list (deleting unnecessary pages)
 pages = pdf.pages[2:26]
+
+text_book = ''
+for page in pages:
+    text_book += page.extract_text()
+
+# Working the text
+final_text = text_book.replace('.', '. ').replace(',', ', ')
+
+
+engine.save_to_file(final_text, 'audiobook.mp3')
+engine.runAndWait()
